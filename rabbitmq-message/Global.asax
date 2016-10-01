3c50d1a7934ddbb5ef0d4c5b2e26b3bd39c323f3
Burak Donbay <burak.donbay@gmail.com>
2016-10-01 16:44:00 +0300

    Add project files.

---
+++ rabbitmq-message/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="rabbitmq_message.MvcApplication" Language="C#" %>
---
+++ rabbitmq-message/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rabbitmq_message.Startup))]
+namespace rabbitmq_message
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
